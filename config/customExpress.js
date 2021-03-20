c072fd758a90a6e2b069f88ff582ef55424a6581
Carlos <carlos.junior@citsamazonas.org.br>
2021-03-20 18:27:04 -0300

    Alteração de cors para validação de header

--- config/customExpress.js
+++ config/customExpress.js
@@ -7,10 +7,22 @@ module.exports = () => {
     const app = express();
 
     //origin: 'http://localhost:4200',
-    var corsOptions = {
-        origin: 'http://demoappfullstack.s3-website-sa-east-1.amazonaws.com/',
-        optionsSuccessStatus: 200
+    // var corsOptions = {
+    //     origin: 'http://demoappfullstack.s3-website-sa-east-1.amazonaws.com/',
+    //     optionsSuccessStatus: 200
+    // }
+    cors: {
+        origin: ["www.one.com","www.two.com","www.three.com"]
     }
+    
+    app.all('*', function(req, res, next) {
+        let origin = req.headers.origin;
+        if(cors.origin.indexOf(origin) >= 0){
+            res.header("Access-Control-Allow-Origin", origin);
+        }         
+        res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+        next();
+    });
 
     app.use(bodyParser.urlencoded({extended: true}));
     app.use(bodyParser.json());
