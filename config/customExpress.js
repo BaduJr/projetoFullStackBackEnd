af41553dfe7bc345764033b8bce5a82f4fe2f9c3
Carlos <carlos.junior@citsamazonas.org.br>
2021-03-20 18:46:43 -0300

    Habilitando CORS sem restringir rota

--- config/customExpress.js
+++ config/customExpress.js
@@ -11,22 +11,10 @@ module.exports = () => {
     //     origin: 'http://demoappfullstack.s3-website-sa-east-1.amazonaws.com/',
     //     optionsSuccessStatus: 200
     // }
-    cors: {
-        origin: ["http://demoappfullstack.s3-website-sa-east-1.amazonaws.com/","http://localhost:4200"]
-    }
-    
-    app.all('*', function(req, res, next) {
-        let origin = req.headers.origin;
-        if(cors.origin.indexOf(origin) >= 0){
-            res.header("Access-Control-Allow-Origin", origin);
-        }         
-        res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-        next();
-    });
 
     app.use(bodyParser.urlencoded({extended: true}));
     app.use(bodyParser.json());
-    //app.use(cors(corsOptions))
+    app.use(cors())
 
     consign()
         .include('controllers')
